0409fa4343b58a9133f273f224bcf0e3dbc96627
Patchethium <asguftuikh@gmail.com>
2025-07-23 02:48:28 +0900

    feat: export class Segment at top level, bump to 0.2.1

--- src/snfa/__init__.py
+++ src/snfa/__init__.py
@@ -1,9 +1,9 @@
 import os.path
 
-from .aligner import Aligner
+from .aligner import Aligner, Segment
 from .utils import trim_audio
 
 # make internal modules invisible
 __path__ = [os.path.dirname(__file__)]
 # re-export the public API
-__all__ = ["Aligner", "trim_audio"]
+__all__ = ["Aligner", "Segment", "trim_audio"]
